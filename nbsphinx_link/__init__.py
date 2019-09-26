6344f9b0c4d10831934b31de31fc1249352aa9ca
Unknown <s.weigand.phy@gmail.com>
2019-09-26 15:46:53 +0200

    implemented changes marked as resolved

--- nbsphinx_link/__init__.py
+++ nbsphinx_link/__init__.py
@@ -11,9 +11,10 @@ The .nblink file is a JSON file with the following structure:
     "path": "relative/path/to/notebook"
 }
 
-Optionally the "extra-media" key can be added, if you notebook includes
-any media, i.e. images. The value can be an array or a string, with
-path/-s to the media file/-s or directory/-s.
+
+Optionally the "extra-media" key can be added, if your notebook includes
+any media, i.e. images. The value needs to be an array of strings,
+which are paths to the media files or directories.
 
 Further keys might be added in the future.
 """
@@ -25,6 +26,7 @@ import shutil
 
 from docutils import io, nodes, utils
 from docutils.utils.error_reporting import SafeString, ErrorString
+from docutils.nodes import document
 from nbsphinx import NotebookParser, NotebookError, _ipynbversion
 import nbformat
 from sphinx.util.logging import getLogger
@@ -36,6 +38,8 @@ def copy_directory(src, dest):
     """
     Copies a directory or file from the path ``src`` to ``dest``.
     Originally from: https://www.pythoncentral.io/how-to-recursively-copy-a-directory-folder-in-python/
+    Which is licensed under the MIT License: https://opensource.org/licenses/MIT
+
 
     Parameters
     ----------
@@ -56,37 +60,49 @@ def copy_directory(src, dest):
             print('Directory not copied. Error: {}'.format(e))
 
 
-def collect_extra_media(extra_media, source_file, nb_path):
+def collect_extra_media(extra_media, source_file, nb_path, document):
     """
     Collects extra media defined in the .nblink file, with the key 'extra-media'.
-    The extra media (i.e. images) need to be copied on order nbsphinx to properly
+    The extra media (i.e. images) need to be copied in order for nbsphinx to properly
     render the notebooks, since nbsphinx assumes that the files are relative to
     the .nblink.
 
     Parameters
     ----------
-    extra_media : str, list
+    extra_media : list
         Path/-s to directorie-/s and/or file-/s with extra media.
     source_file : str
         Path to the .nblink file.
     nb_path : str
         Path to the notebook defined in the .nblink file , with the key 'path'.
+    document: document
+        Parsed document instance.
+
     """
+    logger = getLogger(__name__)
     source_dir = os.path.dirname(source_file)
-    if isinstance(extra_media, str):
-        extra_media = [extra_media]
+    if not isinstance(extra_media, list):
+         logger.warning(
+                'The "extra-media", defined in {} needs to be a list of paths. ' \
+                'The current value is:\n{}'.format(
+                    source_file, extra_media
+                )
+            )
     for extract_media_path in extra_media:
         if os.path.isabs(extract_media_path):
             src_path = extract_media_path
         else:
             extract_media_relpath = os.path.join(source_dir, extract_media_path)
             src_path = os.path.normpath(os.path.join(source_dir, extract_media_relpath))
+
+        document.settings.record_dependencies.add(src_path)
+        document.settings.env.note_dependency(src_path)
+
         target_path = utils.relative_path(nb_path, src_path)
         target_path = os.path.normpath(os.path.join(source_dir, target_path))
         if os.path.exists(src_path):
             copy_directory(src_path, target_path)
         else:
-            logger = getLogger(__name__)
             logger.warning(
                 'The path "{}", defined in {} "extra-media", isn\'t a valid path.'.format(
                     extract_media_path, source_file
@@ -108,9 +124,9 @@ class LinkedNotebookParser(NotebookParser):
         "path": "relative/path/to/notebook"
     }
 
-    Optionally the "extra-media" key can be added, if you notebook includes
-    any media, i.e. images. The value can be an array or a string, with
-    path/-s to the media file/-s or directory/-s.
+    Optionally the "extra-media" key can be added, if your notebook includes
+    any media, i.e. images. The value needs to be an array of strings,
+    which are paths to the media files or directories.
 
     Further keys might be added in the future.
     """
@@ -134,7 +150,7 @@ class LinkedNotebookParser(NotebookParser):
         extra_media = link.get('extra-media', None)
         if extra_media:
             source_file = env.doc2path(env.docname)
-            collect_extra_media(extra_media, source_file, path)
+            collect_extra_media(extra_media, source_file, path, document)
 
         document.settings.record_dependencies.add(path)
         env.note_dependency(path)
