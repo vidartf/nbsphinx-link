a31a7be3c3151b7a6d5d823b763751931f52d8e9
Unknown <s.weigand.phy@gmail.com>
2019-09-27 13:46:45 +0200

    fixed wrong implementation of 'note_reread'
    and fixed copying of files in sub-sub-directory's

--- nbsphinx_link/__init__.py
+++ nbsphinx_link/__init__.py
@@ -33,7 +33,7 @@ from sphinx.util.logging import getLogger
 from ._version import __version__
 
 
-def register_dependency(file_path, document, is_extra_media=False):
+def register_dependency(file_path, document):
     """
     Registers files as dependency, so sphinx rebuilds the docs
     when they changed.
@@ -44,17 +44,12 @@ def register_dependency(file_path, document, is_extra_media=False):
         [description]
     document: docutils.nodes.document
         Parsed document instance.
-    is_extra_media: bool
-        If True triggers docutils.nodes.document.env.note_reread
-        on file_path.
     """
     document.settings.record_dependencies.add(file_path)
     document.settings.env.note_dependency(file_path)
-    if is_extra_media:
-        document.settings.env.note_reread(file_path)
 
 
-def copy_file(src, dest, document, is_extra_media=False):
+def copy_file(src, dest, document):
     """
     Copies a singe file from ``src`` to ``dest``.
 
@@ -66,14 +61,11 @@ def copy_file(src, dest, document, is_extra_media=False):
         Path to the destination file.
     document: docutils.nodes.document
         Parsed document instance.
-    is_extra_media: bool
-        If True triggers docutils.nodes.document.env.note_reread
-        on file_path.
     """
     logger = getLogger(__name__)
     try:
         shutil.copy(src, dest)
-        register_dependency(src, document, is_extra_media=is_extra_media)
+        register_dependency(src, document)
     except (OSError) as e:
         logger.warning(
             "The the file {} couldn't be copied. "
@@ -103,9 +95,9 @@ def copy_and_register_files(src, dest, document):
                 os.makedirs(dst_root)
             for filename in filenames:
                 src_path = os.path.abspath(os.path.join(root, filename))
-                copy_file(src_path, dest, document, is_extra_media=True)
+                copy_file(src_path, dst_root, document)
     else:
-        copy_file(src, dest, document, is_extra_media=True)
+        copy_file(src, dest, document)
 
 
 def collect_extra_media(extra_media, source_file, nb_path, document):
@@ -199,6 +191,7 @@ class LinkedNotebookParser(NotebookParser):
         if extra_media:
             source_file = env.doc2path(env.docname)
             collect_extra_media(extra_media, source_file, path, document)
+            document.settings.env.note_reread()
 
         register_dependency(path, document)
 
